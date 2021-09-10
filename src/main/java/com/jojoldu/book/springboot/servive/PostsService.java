9bb69d6cae9239b824eebfd20c23b6b760999ae7
Hwang inseong <inseongdev@gmail.com>
2021-09-10 14:19:38 +0900

    커밋 210910 14:19

--- src/main/java/com/jojoldu/book/springboot/servive/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/servive/PostsService.java
@@ -26,21 +26,32 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
+
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
         return new PostsResponseDto(entity);
     }
 
     @Transactional
     public List<PostsListResponseDto> findAllDesc() {
-        return postsRepository.findAll().stream()
+        return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
